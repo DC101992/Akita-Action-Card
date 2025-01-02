7d5cae1ca0e293740d67e618b2e6eb86f3fc7617
DC101992 <taggman99@gmail.com>
2025-01-02 11:24:11 -0600

    Add files via upload

---
+++ action graphic.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+# Define the path to your image
+image_path = r'C:\Users\taggm\OneDrive\Desktop\akita action card\akita action card.jpg'
+
+# Open the image
+img = Image.open(image_path)
+
+# Display the image in the default viewer
+img.show()
+
+# If you'd like to save it after modifications, you can do so:
+# img.save('new_image.jpg')
