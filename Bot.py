5af6d3e9a120fb3b5da4d28473f80b221a373a02
DC101992 <taggman99@gmail.com>
2025-01-04 14:11:04 -0600

    Bot update  public key

--- Bot.py
+++ Bot.py
@@ -9,6 +9,9 @@ load_dotenv()
 DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN', 'YOUR_BOT_TOKEN')
 APPLICATION_ID = "1325141241356095591"  # Your application ID
 
+# Channel IDs for the allowed channels
+TARGET_CHANNEL_IDS = [971568747859046420, 904883897224032256]  # Replace with the actual channel IDs
+
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="/", intents=intents, application_id=APPLICATION_ID)
 
@@ -19,7 +22,12 @@ async def on_ready():
 # Register Slash Command
 @bot.slash_command(name="action_card", description="Displays the action card for Akita Dashboard")
 async def action_card(ctx):
-    await ctx.respond("Here's the Action Card for Akita Dashboard!")
+    if ctx.channel.id in TARGET_CHANNEL_IDS:
+        # This will only respond if the command was triggered in the specified channels
+        await ctx.respond("Here's the Action Card for Akita Dashboard!")
+    else:
+        # If the command was issued outside the target channels, do nothing or send a message
+        await ctx.respond("This command can only be used in the designated channels.")
 
 # Run the bot
 bot.run(DISCORD_BOT_TOKEN)
